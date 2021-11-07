f5cf7e2cad3c026d67a56d9285315f6c3d49fcb5
Luccas Santos <luccas.santos@ab-inbev.com>
2021-11-06 22:40:20 -0300

    Adicionando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuHomeComponent } from './menu-home/menu-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CadastrarComponent,
+    LoginComponent,
+    EntrarComponent,
+    MenuHomeComponent
   ],
   imports: [
     BrowserModule,
