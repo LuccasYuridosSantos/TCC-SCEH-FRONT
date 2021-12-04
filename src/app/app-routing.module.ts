1416ad0de6a96817efc735388d6736bd0868def1
Mateus Vaz <mateusv@outlook.com.br>
2021-12-04 16:50:54 -0300

    Adicionando a rota da Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { ContatoComponent } from './contato/contato.component';
-import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
-  {path: 'entrar', component: EntrarComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'contato',component: ContatoComponent },
   {path: 'sobre-nos', component: SobreNosComponent},
   {path: 'cadastrar', component: CadastrarComponent},
